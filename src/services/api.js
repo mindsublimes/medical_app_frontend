ddf022b4c9c51d5ace4b40b1bf8ac9f0897009e5
mindsublimes <mindsublimes@gmail.com>
2024-01-31 21:13:12 +0500

    heroku url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3000/api/v1';
+const API_BASE_URL = 'https://devcache-1dab1743a0c1.herokuapp.com/api/v1';
 
 // Create an instance of Axios with the base URL
 const api = axios.create({ baseURL: API_BASE_URL });
